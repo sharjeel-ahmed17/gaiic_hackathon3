da982b3525f16ac5d740fa46e084039e694d5102
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-02-01 16:05:29 +0500

    ..

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,3 @@
-// 'use client'
-// import { Button } from "@/components/ui/button";
-// import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from "@clerk/nextjs";
 import Category from "@/components/home/category/Category";
 import Cta from "@/components/home/cta/Cta";
 import FeaturedProducts from "@/components/home/featuredProducts/FeaturedProducts";
@@ -10,21 +7,14 @@ import ImageGallery from "@/components/home/imageGallery/ImageGallery";
 
 import { currentUser } from "@clerk/nextjs/server";
 
-// import { useRouter } from "next/navigation";
-
 
 
 export default async function Home() {
-  // const pathname = usePathname;
-  // pathname
 
-  // const router = useRouter();
-
-  // console.log(router.pathname);
 
 
   const user = await currentUser()
-  // console.log(user?.id);
+  console.log(user?.id);
 
   return (
     <div>
--- src/app/(public)/shop/page.tsx
+++ src/app/(public)/shop/page.tsx
@@ -1,9 +1,55 @@
+import ProductListing from '@/components/Product/ProductListing';
+import { Product } from '@/interfaces/Product';
 import React from 'react'
+import ProductToolbar from './Filter';
+import Services from '@/components/reuseable/Services';
+import { Breadcrumb } from '@/components/reuseable/Hero2';
 
-const Shop = () => {
+
+const Shop = async () => {
+    if (!process.env.base_url) {
+        throw new Error("Base Url is not given!.");
+    }
+
+
+
+    const response = await fetch(`${process.env.base_url}/api/product`)
+    if (!response.ok) {
+        throw new Error("Some thing wen wrong");
+    }
+    // create the responsse interface 
+    const { data }: { data: Product[] } = await response.json()
+
+    // console.log(data);
+
+
     return (
-        <div>Shop</div>
+
+        <div>
+            {/* helllo */}
+            <Breadcrumb
+                title="Shop"
+                backgroundImage="/images/hero2.jpg"
+                links={[
+                    { name: "Home", href: "/" },
+                    { name: "Shop", href: "/shop" },
+                ]}
+            />
+            <ProductToolbar />
+            {
+                data.length > 0 ?
+                    <ProductListing products={data} />
+                    : <h1>Product Not Found</h1>
+            }
+            <Services />
+
+
+
+        </div>
     )
 }
 
 export default Shop
+
+
+
