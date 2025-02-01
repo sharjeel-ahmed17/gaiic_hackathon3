1e500175cbcddbc77ecc0ecf168b7966b9bbcf51
blood-donor17 <bdonor13@gmail.com>
2025-02-01 15:20:14 +0500

    update shop page

--- src/components/home/featuredProducts/FeaturedProducts.tsx
+++ src/components/home/featuredProducts/FeaturedProducts.tsx
@@ -1,33 +1,44 @@
-import ProductListing from "@/components/Product/ProductListing";
-import { Button } from "@/components/ui/button";
-import { Product } from "@/interfaces/Product";
-export default  async function FeaturedProducts() {
+// import ProductListing from "@/components/Product/ProductListing";
+// import { Button } from "@/components/ui/button";
+// import { Product } from "@/interfaces/Product";
+// export default  async function FeaturedProducts() {
 
-   if (!process.env.base_url) {
-      throw new Error("Base Url is not given!.");
-  }
-  
- const response = await fetch(`${process.env.base_url}/api/product`)
-    if (!response.ok) {
-      throw new Error("Some thing wen wrong");
-      }
-    // create the responsse interface 
-    const {error,message,data}:{error:boolean,message:string,data:Product[]} = await response.json();
+//    if (!process.env.base_url) {
+//       throw new Error("Base Url is not given!.");
+//   }
+
+//  const response = await fetch(`${process.env.base_url}/api/product`)
+//     if (!response.ok) {
+//       throw new Error("Some thing wen wrong");
+//       }
+//     // create the responsse interface 
+//     const {error,message,data}:{error:boolean,message:string,data:Product[]} = await response.json();
 
 
-  
+
+//   return (
+//     <>
+// {
+//         data.length > 0 ?
+//           <ProductListing products={data.slice(0, 4)}/>  
+//           : <h1>Product Not Found</h1>
+//       }  
+//       <div className="flex justify-center my-5">
+
+
+//    <Button variant="btnPrimary">show more</Button>
+//          </div>
+//     </>
+//   );
+// }
+
+
+import React from 'react'
+
+const FeaturedProducts = () => {
   return (
-    <>
-{
-        data.length > 0 ?
-          <ProductListing products={data.slice(0, 4)}/>  
-          : <h1>Product Not Found</h1>
-      }  
-      <div className="flex justify-center my-5">
-        
-   
-   <Button variant="btnPrimary">show more</Button>
-         </div>
-    </>
-  );
+    <div>FeaturedProducts</div>
+  )
 }
+
+export default FeaturedProducts
