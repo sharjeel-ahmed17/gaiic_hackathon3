620024d6af4d2419d8b25c0324bc0807b3eb4b4c
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-02-06 17:55:34 +0500

    live working properly

--- src/app/(public)/shop/[id]/page.tsx
+++ src/app/(public)/shop/[id]/page.tsx
@@ -4,29 +4,30 @@ import ProductDetailsComp from '@/components/Product/ProductDetails'
 import ProductDescriptionTabBr from '@/components/Product/ProductTabBar'
 
 // Fetch product data from API (Server-Side)
-async function getProductById(id: string) {
-  try {
-    const res = await fetch(`http://localhost:3000/api/product/${id}`, { cache: 'no-store' }); // No cache for fresh data
-    if (!res.ok) throw new Error("Failed to fetch product");
+// async function getProductById(id: string) {
+//   try {
+//     const res = await fetch(`http://localhost:3000/api/product/${id}`, { cache: 'no-store' }); // No cache for fresh data
+//     if (!res.ok) throw new Error("Failed to fetch product");
 
-    const data = await res.json();
-    return data.data; // Extracting product data
-  } catch (error) {
-    console.error("Error fetching product:", error);
-    return null;
-  }
-}
+//     const data = await res.json();
+//     return data.data; // Extracting product data
+//   } catch (error) {
+//     console.error("Error fetching product:", error);
+//     return null;
+//   }
+// }
 // const getProductsById = ()=>{}
-const ProductDetails = async ({ params }: { params: { id: string } }) => {
+// const ProductDetails = async ({ params }: { params: { id: string } }) => {
+const ProductDetails = async () => {
 
-  const product = await getProductById(params.id);
+  // const product = await getProductById(params.id);
 
-  console.log('Product successfully fetched:');
+  // console.log('Product successfully fetched:');
   
 
-  if (!product) {
-    return <div className="text-center text-red-500">Product not found</div>; // Handle 404 gracefully
-  }
+  // if (!product) {
+  //   return <div className="text-center text-red-500">Product not found</div>; // Handle 404 gracefully
+  // }
   
   return (
     <div>
--- src/app/(public)/shop/page.tsx
+++ src/app/(public)/shop/page.tsx
@@ -1,60 +1,69 @@
-import ProductListing from '@/components/Product/ProductListing';
-import { Hero2 } from '@/components/reuseable/Hero2';
-import { Product } from '@/interfaces/Product';
-import React from 'react'
-import ProductToolbar from './Filter';
-import Pagination from '@/components/reuseable/Pagination';
-import Services from '@/components/reuseable/Services';
-const Shop = async ({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) => {
+import ProductListing from "@/components/Product/ProductListing";
+import { Hero2 } from "@/components/reuseable/Hero2";
+import { Product } from "@/interfaces/Product";
+import React from "react";
+import ProductToolbar from "./Filter";
+import Pagination from "@/components/reuseable/Pagination";
+import Services from "@/components/reuseable/Services";
+const Shop = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
   if (!process.env.NEXT_PUBLIC_base_url) {
     throw new Error("Base Url is not given!.");
   }
-const response = await fetch(`${process.env.NEXT_PUBLIC_base_url}/api/product`)
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_base_url}/api/product`
+  );
   if (!response.ok) {
     throw new Error("Some thing went wrong");
   }
-  const { error, message, data }: { error: boolean, message: string, data: Product[] } = await response.json()
+  const {
+    error,
+    message,
+    data,
+  }: { error: boolean; message: string; data: Product[] } =
+    await response.json();
 
   if (error) {
     throw new Error(message);
   }
 
+  const searchParam = await searchParams;
 
-  const searchParam = await searchParams
+  const page = searchParam.page || "1";
+  const perPage = searchParam.perPage || "8";
+  const sortBy = searchParam.sortBy || "default";
 
-  const page = searchParam.page || '1'
-  const perPage = searchParam.perPage || '8'
-  const sortBy = searchParam.sortBy || 'default'
-
-
-  
-  const totalPages = Math.ceil(data.length / Number(perPage)) 
-  const offsetStart = (Number(page) - 1) * Number(perPage) 
-  const offsetEnd = offsetStart + Number(perPage)
-const sortedData = [...data].sort((a, b) =>
-    sortBy === "price-asc" ? a.discountPrice - b.discountPrice :  
-      sortBy === "price-desc" ? b.discountPrice - a.discountPrice :  
-        
-        sortBy === "newest" ? Number(new Date(b._createdAt)) - Number(new Date(a._createdAt)) :  
-          0 
+  const totalPages = Math.ceil(data.length / Number(perPage));
+  const offsetStart = (Number(page) - 1) * Number(perPage);
+  const offsetEnd = offsetStart + Number(perPage);
+  const sortedData = [...data].sort((a, b) =>
+    sortBy === "price-asc"
+      ? a.discountPrice - b.discountPrice
+      : sortBy === "price-desc"
+        ? b.discountPrice - a.discountPrice
+        : sortBy === "newest"
+          ? Number(new Date(b._createdAt)) - Number(new Date(a._createdAt))
+          : 0
   );
 
   return (
-
     <div>
-      <Hero2 title="Shop"/>
+      <Hero2 title="Shop" />
       <ProductToolbar totalLength={data.length} />
-      {
-        data.length > 0 ?
-          <ProductListing products={sortedData.slice(offsetStart, offsetEnd)} />
-          : <h1>No Product Found</h1>
-      }
+      {data.length > 0 ? (
+        <ProductListing products={sortedData.slice(offsetStart, offsetEnd)} />
+      ) : (
+        <h1>No Product Found</h1>
+      )}
 
       <Pagination totalPages={totalPages} />
 
       <Services />
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
