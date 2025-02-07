a2f6064bd68390176c30acc75b95dee7fcd39ea8
blood-donor17 <bdonor13@gmail.com>
2025-02-07 16:19:59 +0500

    live

--- src/app/(public)/blog/page.tsx
+++ src/app/(public)/blog/page.tsx
@@ -1,16 +1,16 @@
 "use client"
-import { Hero2 } from '@/components/reuseable/Hero2'
+import { Hero2 } from '@/components/resuable/Hero2'
 import { useCart } from '@/contexts/cartContext'
 import React from 'react'
 import BlogPage from './BlogPage'
 
 const Blog = () => {
-  const {cart}  = useCart();
-  console.log('cart',cart);
-  
+  const { cart } = useCart();
+  console.log('cart', cart);
+
   return (
     <div>
- <Hero2 title='Blog'/>
+      <Hero2 title='Blog' />
       <BlogPage />
     </div>
   )
--- src/components/Product/ProductDetails.tsx
+++ src/components/Product/ProductDetails.tsx
@@ -1,28 +1,20 @@
-
 'use client'
 import { Product } from "@/interfaces/Product";
 import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
-import { useCart } from '@/context/CartContext'
+import { useCart } from '@/contexts/cartContext'
 import { useEffect, useState } from "react";
 import { cartObject } from "@/lib/object";
 import { Star } from "lucide-react";
 
 export default function ProductDetailsComp({ ProductDetail }: { ProductDetail: Product }) {
-    //use the state to change the image if user selected that image
     const [selectedImage, setSelectedImage] = useState(ProductDetail.thumbnail)
-    // console.log(selectedImage);
-
     const { addProdToCart, cart } = useCart()
-    const [addedToCart, setAddedToCart] = useState(false) //use state to show if item is already in cart or not
-
-    // create hanlde cart to store data in cart first pass from cartobj
+    const [addedToCart, setAddedToCart] = useState(false)
     const handleCart = (prod: Product) => {
         const obj = cartObject(prod)
         addProdToCart(obj)
     }
-
-    //use useeffect for any change in cart context cause the addedToCart become true and not allow again to store in cart
     useEffect(() => {
         const findProdInCart = cart.find((c) => c.productId === ProductDetail._id)
         if (findProdInCart) {
@@ -33,7 +25,7 @@ export default function ProductDetailsComp({ ProductDetail }: { ProductDetail: P
 
     return (
         <div className="flex flex-col lg:flex-row gap-8 p-6 max-w-7xl mx-auto">
-            {/* Left Side - Image Gallery */}
+
             <div className="flex gap-4 lg:w-1/2 flex-col md:flex-row">
                 <div className="flex md:flex-col flex-row gap-4 order-2 md:order-1">
                     {
@@ -51,7 +43,7 @@ export default function ProductDetailsComp({ ProductDetail }: { ProductDetail: P
                 </div>
             </div>
 
-            {/* Right Side - Product Info */}
+
             <div className="flex flex-col gap-4 lg:w-1/2">
                 <h1 className="text-3xl font-bold">{ProductDetail.name}</h1>
                 <p className="text-gray-500 text-lg">Rs. {ProductDetail.discountPrice > 0 ? ProductDetail.discountPrice : ProductDetail.price}</p>
@@ -69,8 +61,6 @@ export default function ProductDetailsComp({ ProductDetail }: { ProductDetail: P
                         ProductDetail.description
                     }
                 </p>
-
-                {/* Size Selection */}
                 <div>
                     <h2 className="text-sm font-semibold">Size</h2>
                     <div className="flex gap-2 mt-1 ">
@@ -82,12 +72,11 @@ export default function ProductDetailsComp({ ProductDetail }: { ProductDetail: P
 
                             ))
                         }
-                        {/* <button className="px-3 py-1 border rounded-lg">XL</button>
-                        <button className="px-3 py-1 border rounded-lg">XS</button> */}
+
                     </div>
                 </div>
 
-                {/* Color Selection */}
+
                 <div>
                     <h2 className="text-sm font-semibold">Color</h2>
                     <div className="flex gap-2 mt-1">
@@ -98,26 +87,19 @@ export default function ProductDetailsComp({ ProductDetail }: { ProductDetail: P
                                     style={{ background: color }} key={ind}></span>
                             ))
                         }
-                        {/* <span className="w-6 h-6 rounded-full bg-black block"></span>
-                        <span className="w-6 h-6 rounded-full bg-yellow-600 block"></span> */}
+
                     </div>
                 </div>
 
-                {/* Quantity and Buttons */}
+
                 <div className="flex items-center gap-4 mt-4 flex-col md:flex-row">
-                    {/* <div className="flex items-center border rounded-lg">
-                        <button className="px-3 py-2">-</button>
-                        <span className="px-4">1</span>
-                        <button className="px-3 py-2">+</button>
-                    </div> */}
+
                     <button className={`px-6 py-3 border rounded-lg ${addedToCart ? 'bg-yellow-600' : ''}`}
                         disabled={addedToCart} onClick={() => handleCart(ProductDetail)}>
                         Add To Cart
                     </button>
                     <button className="px-6 py-3 border rounded-lg">+ Compare</button>
                 </div>
-
-                {/* Product Meta Info */}
                 <div className="text-gray-500 text-sm mt-4">
                     <p><strong>SKU:</strong> {ProductDetail.sku}</p>
                     <p><strong>Category:</strong> {ProductDetail.category._ref}</p>
