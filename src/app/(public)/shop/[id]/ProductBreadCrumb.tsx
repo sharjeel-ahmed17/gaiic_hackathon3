2c6e50318679e53ba827b4219c478d73ba57ae9f
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-02-07 10:02:47 +0500

    product_details

--- src/app/(public)/shop/[id]/ProductBreadCrumb.tsx
+++ src/app/(public)/shop/[id]/ProductBreadCrumb.tsx
@@ -1,14 +1,13 @@
-export default function Breadcrumb() {
-    return (
+export default function Breadcrumb({ prodName }: { prodName: string }) {
+  return (
       <nav className="bg-cream p-4">
-        <div className="flex items-center text-gray-500 text-sm">
-          <a href="#" className="hover:text-black">Home</a>
-          <span className="mx-2">›</span>
-          <a href="#" className="hover:text-black">Shop</a>
-          <span className="mx-2">›</span>
-          <span className="text-black font-semibold">Asgaard sofa</span>
-        </div>
+          <div className="flex items-center text-gray-500 text-sm">
+              <a href="#" className="hover:text-black">Home</a>
+              <span className="mx-2">›</span>
+              <a href="#" className="hover:text-black">Shop</a>
+              <span className="mx-2">›</span>
+              <span className="text-black font-semibold">{prodName}</span>
+          </div>
       </nav>
-    );
-  }
-  
+  );
+}
